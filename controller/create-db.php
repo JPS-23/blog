282a41512d275d5f5d913e956e7d54246f4d1686
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-10-30 11:22:20 -0700

    finished video 15

--- controller/create-db.php
+++ controller/create-db.php
@@ -4,7 +4,7 @@
     $connection = new mysqli($host, $username, $password); //this code holds some variables used in the if statement
     
     if($connection->connect_error) {
-        die("Error: " . $connection->connect_error); //still not sure what this does
+        die("<p>Error: " . $connection->connect_error . "</p>"); //still not sure what this does
     }
     
     $exists = $connection->select_db($database); //This code checks that $database exists
@@ -13,7 +13,7 @@
        $query = $connection->query("CREATE DATABASE $database"); //allows to perform actions on database
        
        if($query) {
-        echo "Successfully created database" . $database;   
+        echo "<p>Successfully created database" . $database . "</p>";   
        }
     }
     else {
@@ -21,9 +21,16 @@
     }
     
     $query = $connection->query("CREATE TABLE posts (" // we are creating id's for the tables in localhost
-            . "id int(11) NOT NULL AUTO_INCREMENT,"
+            . "id int(11) NOT NULL AUTO_INCREMENT," //there is up to 11 values
             . "title varchar(255) NOT NULL," //this says there is at least 255 charectors for the title
-            . "post text NOT NULL,"
-            . "PRIMARY KEY ()"); //this contains all the text, its not a varchar
+            . "post text NOT NULL," //we create a post here, posts cant be empty
+            . "PRIMARY KEY (id))"); //this contains all the text, its not a varchar
+    
+    if($query) {
+        echo "<p>Successfully created table posts</p>"; // with this code we have created a file in the database folder
+    }
+    else {
+        echo "<p>$connection->error</p>"; //this will check if query is false
+    }
     
     $connection-> close();
