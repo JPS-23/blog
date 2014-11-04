d12c08efe093b18f3b2c8d720cf8e54dc0207a07
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-11-04 11:12:04 -0800

    finished video 17

--- index.php
+++ index.php
@@ -1,8 +1,6 @@
 <?php
-    require_once(__DIR__ . "/view/header.php");
+    require_once(__DIR__ . "/view/header.php");//all this code opens up the specific folders its tld to open
+    require_once(__DIR__ . "/view/navigation.php");
     require_once(__DIR__ . "/controller/create-db.php");
     require_once(__DIR__ . "/view/footer.php");
 ?>
-
-
-
--- view/navigation.php
+++ view/navigation.php
@@ -1,8 +1,5 @@
-<?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+<nav>
+    <ul>
+        <li><a href="../post.php">Blog Post Form</a></li> <!--this creates a link in the webpage-->
+    </ul>
+</nav>
