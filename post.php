77d739dff45565687bd56fdbf3a58b5b5fffa1b5
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-11-05 11:23:38 -0800

    finished video 19

--- post.php
+++ post.php
@@ -1,8 +1,5 @@
 <?php
-
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
+    require_once(__DIR__ . "/view/header.php");//this opens the header file
+    require_once(__DIR__ . "/view/form.php");//we point to the correct php file here
+    require_once(__DIR__ . "/view/footer.php");//here we open the footer file
+?>
---
+++ view/form.php
@@ -0,0 +1,14 @@
+<h1>Create Blog Post</h1><!--this indicates what the folder is for-->
+
+<form><!--the form code takes in information-->
+    <div>
+        <label for="title">Title: </label><!--this is for the title-->
+        <input type="text" name="title" /> <!--the input box is usually on one line-->
+    </div>
+    
+    <div>
+        <label for="post">Post: </label><!--this is just one small area to type in-->
+        <textarea name="post"></textarea><!--you can resize this on the webpage-->
+        
+    </div>
+</form>
