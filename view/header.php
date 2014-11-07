80cd078450457a8fb2503f765a0cf0cd0db1c422
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-11-07 10:14:03 -0800

    finished video 21

--- view/header.php
+++ view/header.php
@@ -1,5 +1,4 @@
 <!DOCTYPE html>
-<meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
 <!--
 To change this license header, choose License Headers in Project Properties.
 To change this template file, choose Tools | Templates
@@ -8,6 +7,7 @@ and open the template in the editor.
 <html> <!-- this is how every html code should start -->
     <head>
         <meta charset="UTF-8">
+        <meta name="viewport" content="minimal-ui, width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
         <title></title>
     </head>
     <body>
