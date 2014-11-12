fe298a4e848d510cdd3c79581b0aaf3e25ef9587
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-11-12 11:27:00 -0800

    i finished video 23; i have established a database connection  through  create-post.php

--- controller/create-post.php
+++ controller/create-post.php
@@ -1,8 +1,13 @@
-<?ph<?php
+<?php
+    require_once(__DIR__ . "/../model/database.php");
+    
+    $connection = new mysqli($host, $username, $password, $database);
+    
     $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING );//this post says were are sending and recieving information
     $post = filter_input(INPUT_POST, "post", FILTER_SANITIZE_STRING );//we are also adding a filter so we can protect our code
     
     echo "<p>Title: $title</p>";//this echos out our title
     echo "<p>Post: $post<p/>";//this also echos out our post
     
+    $connection->close();
     
