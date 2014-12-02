cbff4f54c5817d25c843d6ba18da42fb8780a7cd
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-12-02 11:22:30 -0800

    finished video 37

---
+++ view/register-form.php
@@ -0,0 +1,21 @@
+<form>
+    <div>
+        <label for="email">Email: </label><!-- this label is for the email's -->
+        <input type="text" name="email" />
+    </div>
+    
+    <div>
+        <label for="username">Username: </label><!-- this label is for the usernames -->
+        <input type="text" name="username" />
+    </div>
+    
+    <div>
+        <label for="password">Password: </label><!-- this label is for the passwords -->
+        <input type="password" name="password" />
+    </div>
+    
+    <div>
+        <button type="submit">Submit</button>
+    </div>
+</form>
+
