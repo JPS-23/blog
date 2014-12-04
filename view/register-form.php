e94c07e73d96498d96cab9ef5511762b39e54291
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-12-04 11:15:22 -0800

    finished video 39; attaching a controller to the Register form

--- view/register-form.php
+++ view/register-form.php
@@ -1,4 +1,10 @@
-<form>
+<?php
+    require_once (__DIR__ . "/../model/config.php");
+?>
+
+<h1>Register</h1>
+
+<form method="post" action="<?php echo $path . "controller/create-user.php"; ?>"><!-- this is to send information to the database -->
     <div>
         <label for="email">Email: </label><!-- this label is for the email's -->
         <input type="text" name="email" />
