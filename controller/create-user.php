e32bf90f430c1c67899a6909d87aea0aa597cceb
Josue Peralta-Soriano <peralta.josue09@gmail.com>
2014-12-08 11:58:11 -0800

    finished video 43; creating a salt for our password through create-user.php

--- controller/create-user.php
+++ controller/create-user.php
@@ -1,8 +1,12 @@
 <?php
     require_once (__DIR__ . "/../model/config.php");
     
-    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
-    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);
+    $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);//all these just filter our code
+    $username = filter_input(INPUT_POST, "username", FILTER_SANITIZE_STRING);//and sanitize it
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_STRING);
     
-    echo $email . " - " . $username . " - " . $password;
+    echo $password;
+    
+    $salt = "$5$" . "rounds=5000$" . uniqid(mt_rand(), true) . "$"; //this is so we can create a unique id
+    
+    echo $salt;
